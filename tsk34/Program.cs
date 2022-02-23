abf3583adf47feac3875b2a96e0fec50daff70c6
Artem <40538552+surge603@users.noreply.github.com>
2022-02-23 22:25:36 +0300

    Задача 34

---
+++ tsk34/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 34: Написать программу для замены элементов массива на противоположные.
+
+Console.Clear();
+int[] arr = new int[10];
+Console.WriteLine("Исходний массив:");
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(-100, 100);
+    Console.Write(arr[i]+" ");
+}
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Новый массив:");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = -arr[i];
+    Console.Write(arr[i]+" ");
+}
+Console.WriteLine();
